13280c88a60fe4ab8a11ac99723d3e8bb45e6d1e
zhalgas-seidazym <124429967+zhalgas-seidazym@users.noreply.github.com>
2023-02-04 15:20:51 +0600

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,5 +1,5 @@
 public class Circle {
-    double radius = 1.0;
+    private double radius = 1.0;
 
     public Circle(double radius){
         this.radius = radius;
@@ -19,5 +19,12 @@ public class Circle {
     @Override
     public String toString(){
         return "Circle[radius=" + radius + "]";
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+        Circle c = new Circle(3.0);
+        System.out.println(c.toString() + " " + "\n" + "Area:" + c.getArea() + "\n" + "Circumference:" + c.getCircumference());
     }
 }
